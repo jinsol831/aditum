fcf5d6e06b3cbe7086005784c1100bec5caf07ba
Ivelisse Cintron <33500791+ivelissecintron@users.noreply.github.com>
2018-08-30 20:24:31 -0500

    updated logo

--- aditum/client_src/test-admin/src/components/Dashboard.js
+++ aditum/client_src/test-admin/src/components/Dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
+import logo from '../logo.png';
 
 export default () => (
     <Card>
-        <CardHeader title="Welcome to the administration" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+        <CardHeader title="Welcome to the Aditum Dashboard" />
+        <img src={logo} className="App-logo" alt="logo" />
     </Card>
 );
